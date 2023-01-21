419cb2c5a3cb99e99ab286cc5d1cf5a68f6701d9
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-21 20:06:11 -0300

    Adicionado componente Spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+interface SpinnerProps {
+  size: number
+}
+
+const Spinner = ({ size = 32 }: SpinnerProps) => {
+  return <S.StyledSpinner size={size} />
+}
+
+export default Spinner
