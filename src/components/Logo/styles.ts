4639234209da5dd4e58ef978f3765b4c88e4fa02
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-24 20:41:54 -0300

    Adicionado componente Logo

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 13rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 0.1rem solid rgba(255, 255, 255, 0.1);
+`
