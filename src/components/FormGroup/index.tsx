daee96eebddd12687350b109bed76dd59c9edf13
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-23 23:06:18 -0300

    Refatorado FormGroup

--- src/components/FormGroup/index.tsx
+++ src/components/FormGroup/index.tsx
@@ -3,17 +3,27 @@ import * as S from './styles'
 interface FormGroupProps {
   children: React.ReactNode
   label: string
+  error?: string
   required?: boolean
+  password?: boolean
 }
 
-const FormGroup = ({ children, label, required }: FormGroupProps) => {
+const FormGroup = ({
+  children,
+  label,
+  error,
+  required,
+  password
+}: FormGroupProps) => {
   return (
-    <S.Label>
+    <S.Label password={password}>
       <p>
         {label}
         {required && <span>*</span>}
       </p>
       {children}
+
+      {error && <S.Error>{error}</S.Error>}
     </S.Label>
   )
 }
--- src/components/FormGroup/styles.ts
+++ src/components/FormGroup/styles.ts
@@ -1,9 +1,14 @@
 import styled, { css } from 'styled-components'
 
-export const Label = styled.label`
-  ${({ theme }) => css`
+interface FormGroupStyleProps {
+  password?: boolean
+}
+
+export const Label = styled.label<FormGroupStyleProps>`
+  ${({ theme, password }) => css`
     display: block;
     margin-bottom: 2.4rem;
+    position: relative;
 
     p {
       margin: 0 0 1.3rem 0;
@@ -14,5 +19,40 @@ export const Label = styled.label`
         color: ${theme.colors.secondary};
       }
     }
+
+    svg {
+      color: ${theme.colors.lightGray};
+    }
+
+    ${password &&
+    css`
+      div {
+        position: relative;
+      }
+
+      input {
+        padding: 0 4.5rem 0 2.4rem;
+      }
+
+      button {
+        background: transparent;
+        border: 0;
+      }
+
+      svg {
+        cursor: pointer;
+        position: absolute;
+        right: 1.3rem;
+        bottom: 1.4rem;
+      }
+    `}
+  `}
+`
+
+export const Error = styled.span`
+  ${({ theme }) => css`
+    display: block;
+    color: ${theme.colors.error};
+    margin-top: 0.5rem;
   `}
 `
