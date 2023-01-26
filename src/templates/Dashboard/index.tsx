e4b3c4fec88d2592ec3ff3e26da685d9a0a1d2f2
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-26 00:21:59 -0300

    Adicionado template Dashboard

---
+++ src/templates/Dashboard/index.tsx
@@ -0,0 +1,61 @@
+import Base from '../Base'
+
+const Dashboard = () => {
+  return (
+    <Base title="Dashboard" titleBreadcrumb="Dashboard">
+      conteudo
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </Base>
+  )
+}
+
+export default Dashboard
