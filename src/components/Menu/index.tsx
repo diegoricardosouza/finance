53acbff258c7d93c13bc7ab5800bb06f9a1cda4e
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-25 23:29:03 -0300

    Adicionado open e close Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,77 +3,99 @@ import {
   MdHome,
   MdOutlineShoppingCart,
   MdTableChart,
-  MdLock
+  MdLock,
+  MdOutlineClose,
+  MdOutlineMenu
 } from 'react-icons/md'
 import { TbLayoutKanban } from 'react-icons/tb'
 import { ImUser } from 'react-icons/im'
 
 import Logo from '../Logo'
 import * as S from './styles'
+import { useState } from 'react'
 
 const Menu = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <S.Container>
-      <Logo />
+    <>
+      <S.Overlay open={open} />
+      <S.ButtonOpenMenu>
+        <button onClick={() => setOpen(!open)}>
+          <MdOutlineMenu size={28} />
+        </button>
+      </S.ButtonOpenMenu>
 
-      <S.WrapperMenu>
-        <ul>
-          <S.ItemMenu active>
-            <Link href="/">
-              <S.IconMenu>
-                <MdHome size={20} />
-              </S.IconMenu>
-              Dashboard
-            </Link>
-          </S.ItemMenu>
+      <S.Container open={open}>
+        <S.WrapperLogo>
+          <S.ButtonClose>
+            <button onClick={() => setOpen(!open)}>
+              <MdOutlineClose size={24} />
+            </button>
+          </S.ButtonClose>
 
-          <S.ItemMenu>
-            <Link href="/">
-              <S.IconMenu>
-                <MdOutlineShoppingCart size={20} />
-              </S.IconMenu>
-              NFT Marketplace
-            </Link>
-          </S.ItemMenu>
+          <Logo />
+        </S.WrapperLogo>
 
-          <S.ItemMenu>
-            <Link href="/">
-              <S.IconMenu>
-                <MdTableChart size={20} />
-              </S.IconMenu>
-              Tables
-            </Link>
-          </S.ItemMenu>
+        <S.WrapperMenu>
+          <ul>
+            <S.ItemMenu active>
+              <Link href="/">
+                <S.IconMenu>
+                  <MdHome size={20} />
+                </S.IconMenu>
+                Dashboard
+              </Link>
+            </S.ItemMenu>
 
-          <S.ItemMenu>
-            <Link href="/">
-              <S.IconMenu>
-                <TbLayoutKanban size={20} />
-              </S.IconMenu>
-              Kanban
-            </Link>
-          </S.ItemMenu>
+            <S.ItemMenu>
+              <Link href="/">
+                <S.IconMenu>
+                  <MdOutlineShoppingCart size={20} />
+                </S.IconMenu>
+                NFT Marketplace
+              </Link>
+            </S.ItemMenu>
 
-          <S.ItemMenu>
-            <Link href="/">
-              <S.IconMenu>
-                <ImUser size={20} />
-              </S.IconMenu>
-              Profile
-            </Link>
-          </S.ItemMenu>
+            <S.ItemMenu>
+              <Link href="/">
+                <S.IconMenu>
+                  <MdTableChart size={20} />
+                </S.IconMenu>
+                Tables
+              </Link>
+            </S.ItemMenu>
 
-          <S.ItemMenu>
-            <Link href="/">
-              <S.IconMenu>
-                <MdLock size={20} />
-              </S.IconMenu>
-              Sign In
-            </Link>
-          </S.ItemMenu>
-        </ul>
-      </S.WrapperMenu>
-    </S.Container>
+            <S.ItemMenu>
+              <Link href="/">
+                <S.IconMenu>
+                  <TbLayoutKanban size={20} />
+                </S.IconMenu>
+                Kanban
+              </Link>
+            </S.ItemMenu>
+
+            <S.ItemMenu>
+              <Link href="/">
+                <S.IconMenu>
+                  <ImUser size={20} />
+                </S.IconMenu>
+                Profile
+              </Link>
+            </S.ItemMenu>
+
+            <S.ItemMenu>
+              <Link href="/">
+                <S.IconMenu>
+                  <MdLock size={20} />
+                </S.IconMenu>
+                Sign In
+              </Link>
+            </S.ItemMenu>
+          </ul>
+        </S.WrapperMenu>
+      </S.Container>
+    </>
   )
 }
 
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,16 +1,39 @@
+import { device } from '@/utils/device'
 import styled, { css } from 'styled-components'
 
 interface MenuStyleProps {
   active?: boolean
 }
 
-export const Container = styled.div`
-  ${({ theme }) => css`
+interface OpenMenuProps {
+  open: boolean
+}
+
+export const Container = styled.div<OpenMenuProps>`
+  ${({ theme, open }) => css`
     background-color: ${theme.colors.primaryLight};
     height: 100vh;
     width: 29rem;
     overflow: hidden;
+    position: relative;
+    transition: all 0.2s ease-in;
+    left: ${open ? 0 : '-100%'};
+    z-index: 6;
+
+    @media ${device.laptop} {
+      left: 0;
+    }
   `}
+`
+
+export const WrapperLogo = styled.div`
+  width: 100%;
+  height: 13rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-bottom: 0.1rem solid rgba(255, 255, 255, 0.1);
 `
 
 export const WrapperMenu = styled.nav`
@@ -33,7 +56,7 @@ export const WrapperMenu = styled.nav`
 
   /* Track */
   ::-webkit-scrollbar-track {
-    background: transparent;
+    background: rgba(0, 0, 0, 0.4);
   }
 
   /* Handle */
@@ -90,4 +113,66 @@ export const IconMenu = styled.div`
   align-items: center;
   justify-content: center;
   margin-right: 1.5rem;
+`
+
+export const ButtonClose = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+
+    button {
+      background: transparent;
+      border: 0;
+      cursor: pointer;
+      margin: 0 2rem 3rem 0;
+
+      svg {
+        color: ${theme.colors.white};
+      }
+    }
+
+    @media ${device.laptop} {
+      display: none;
+    }
+  `}
+`
+
+export const Overlay = styled.div<OpenMenuProps>`
+  ${({ open }) => css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: all 0.2s ease-in;
+    z-index: 6;
+    background: rgba(0, 0, 0, 0.48);
+    opacity: ${open ? 1 : 0};
+    pointer-events: ${open ? 'inherit' : 'none'};
+    cursor: none;
+  `}
+`
+
+export const ButtonOpenMenu = styled.div`
+  ${({ theme }) => css`
+    position: fixed;
+    top: 4.1rem;
+    right: 2rem;
+    z-index: 5;
+
+    button {
+      background: transparent;
+      border: 0;
+      cursor: pointer;
+
+      svg {
+        color: ${theme.colors.white};
+      }
+    }
+
+    @media ${device.laptop} {
+      display: none;
+    }
+  `}
 `
