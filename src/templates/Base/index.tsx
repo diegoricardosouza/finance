5851f244cb1f54565db70ed7486372ebf7ac643a
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-26 00:19:13 -0300

    Adicionado template Base

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header'
+import Menu from '@/components/Menu'
+import { BaseProps } from '@/utils/types'
+
+import * as S from './styles'
+
+const Base = ({ children, title, titleBreadcrumb }: BaseProps) => {
+  return (
+    <S.Container>
+      <Menu />
+
+      <S.Content>
+        <Header title={title} titleBreadcrumb={titleBreadcrumb} />
+        {children}
+      </S.Content>
+    </S.Container>
+  )
+}
+
+export default Base
---
+++ src/templates/Base/styles.ts
@@ -0,0 +1,45 @@
+import { device } from '@/utils/device'
+import styled, { css } from 'styled-components'
+
+export const Container = styled.main``
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    height: 100vh;
+    overflow: auto;
+    overflow-x: hidden;
+    padding: 12rem 2rem 0 2rem;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+
+    /* width */
+    ::-webkit-scrollbar {
+      width: 0.6rem;
+      transition: all 0.2s ease-in;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: rgba(0, 0, 0, 0.4);
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: ${theme.colors.secondary};
+      border-radius: 10rem;
+      transition: all 0.2s ease-in;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: ${theme.colors.secondaryHover};
+    }
+
+    @media ${device.laptop} {
+      width: calc(100% - 29rem);
+      padding-top: 14.5rem;
+    }
+  `}
+`
