105552dffe45b47d5f5440e4e0131c9ce239080e
Diego Ricardo <diegoricardoweb@gmail.com>
2023-01-28 23:42:13 -0300

    Adicionado utilitario de ano e mes

---
+++ src/utils/months.ts
@@ -0,0 +1,50 @@
+export const months = [
+  {
+    title: 'Jan',
+    value: 'janeiro'
+  },
+  {
+    title: 'Fev',
+    value: 'fevereiro'
+  },
+  {
+    title: 'Mar',
+    value: 'marco'
+  },
+  {
+    title: 'Abr',
+    value: 'abril'
+  },
+  {
+    title: 'Mai',
+    value: 'maio'
+  },
+  {
+    title: 'Jun',
+    value: 'junho'
+  },
+  {
+    title: 'Jul',
+    value: 'julho'
+  },
+  {
+    title: 'Ago',
+    value: 'agosto'
+  },
+  {
+    title: 'Set',
+    value: 'setembro'
+  },
+  {
+    title: 'Out',
+    value: 'outubro'
+  },
+  {
+    title: 'Nov',
+    value: 'novembro'
+  },
+  {
+    title: 'Dez',
+    value: 'dezembro'
+  }
+]
---
+++ src/utils/years.ts
@@ -0,0 +1,14 @@
+export const years = [
+  {
+    title: '2021',
+    value: '2021'
+  },
+  {
+    title: '2022',
+    value: '2022'
+  },
+  {
+    title: '2023',
+    value: '2023'
+  }
+]
