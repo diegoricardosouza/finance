89d1ba117099624d15eb260a3a00588748748eba
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-04 00:48:29 -0300

    Adicionado mock de items e categorias

---
+++ src/data/items.ts
@@ -0,0 +1,32 @@
+import { Item } from '@/types/Item'
+
+export const items: Item[] = [
+  {
+    id: 'sdafasdfsa-123sa1df32a1',
+    date: new Date(2023, 1, 20),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    id: 'wqerty78-789as7df',
+    date: new Date(2023, 1, 22),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300
+  },
+  {
+    id: 'xzcv4f56as-4asdf-78a9s',
+    date: new Date(2023, 1, 25),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  {
+    id: 'qwdva-qwerft-fiadpm-4va622',
+    date: new Date(2023, 1, 26),
+    category: 'salary',
+    title: 'Salário',
+    value: 3300
+  }
+]
