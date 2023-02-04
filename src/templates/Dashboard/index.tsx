8f017415cbe6e84e5f9ca9f8b28fc34e077dc514
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-04 15:22:33 -0300

    Ajustado template Dashboard

--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,56 +1,26 @@
+import { useState } from 'react'
 import { MdOutlineAttachMoney } from 'react-icons/md'
 import { GiMoneyStack, GiTakeMyMoney } from 'react-icons/gi'
 
-import Card from '@/components/Card'
 import Base from '../Base'
+import Card from '@/components/Card'
 import SelectedData from '@/components/SelectedData'
-import Table from '@/components/Table'
+import { currentMonth } from '@/utils/dateFilter'
 
 import * as S from './styles'
 
-const columns = [
-  {
-    title: 'Data',
-    width: 170
-  },
-  {
-    title: 'Categoria',
-    width: 170
-  },
-  {
-    title: 'Título'
-  },
-  {
-    title: 'Valor',
-    width: 170
-  }
-]
-
-const data = [
-  {
-    id: 'sdafasdfsa-123sa1df32a1',
-    date: '2022-10-20',
-    category: 'Alimentação',
-    title: 'McDonalds',
-    value: 32.12
-  },
-  {
-    id: 'wqerty78-789as7df',
-    date: '2022-10-22',
-    category: 'Aluguel',
-    title: 'Aluguel Apt',
-    value: 2300
-  },
-  {
-    id: 'xzcv4f56as-4asdf-78a9s',
-    date: '2022-10-25',
-    category: 'Alimentação',
-    title: 'Burger King',
-    value: 28
-  }
-]
-
 const Dashboard = () => {
+  const [selectedMonth, setSelectedMonth] = useState('')
+  const [selectedYear, setSelectedYear] = useState('')
+  const date = new Date()
+
+  const handleFilter = ({ month, year }: { month: string; year: string }) => {
+    setSelectedMonth(month === '' ? currentMonth() : month)
+    setSelectedYear(year === '' ? String(date.getFullYear()) : year)
+  }
+
+  // console.log('click', selectedMonth, selectedYear)
+
   return (
     <Base title="Dashboard" titleBreadcrumb="Dashboard">
       <S.Container>
@@ -75,17 +45,8 @@ const Dashboard = () => {
           color="green"
         />
 
-        <SelectedData />
+        <SelectedData onButtonClick={handleFilter} />
       </S.Container>
-
-      <S.WrapperTable>
-        <Table
-          title="Development Table"
-          columns={columns}
-          data={data}
-          deleteButton
-        />
-      </S.WrapperTable>
     </Base>
   )
 }
