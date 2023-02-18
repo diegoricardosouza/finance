13b35d8775a949410e163fc27a8985267b39af1a
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-18 00:30:08 -0300

    Adicionado util de senha

---
+++ src/utils/passwordHash.ts
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt'
+
+const saltRounds = 10
+
+export const hashPassword = async (password: string): Promise<string> => {
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hash = await bcrypt.hash(password, salt)
+  return hash
+}
+
+export const comparePassword = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
+  const match = await bcrypt.compare(password, hash)
+  return match
+}
