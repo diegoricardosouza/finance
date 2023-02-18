83ad19c4ecd3ad138ed1931450ea77bbfedf5ed5
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-18 17:04:47 -0300

    Verificando se está logado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import Login from '@/templates/Login'
+import { GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
 
 export default function Home() {
   return <Login />
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      },
+      props: { session }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
