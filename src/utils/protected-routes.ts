ac12b7aefdae800afd21d9e69bb0e144178840ed
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-18 06:37:53 -0300

    Adicionado utils de proteção de rota

---
+++ src/utils/protected-routes.ts
@@ -0,0 +1,15 @@
+import { GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
+
+async function protectedRoutes(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    context.res.setHeader('Location', `/?callbackUrl=${context.resolvedUrl}`)
+
+    context.res.statusCode = 302
+  }
+  return session
+}
+
+export default protectedRoutes
