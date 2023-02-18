5166f796ef25315ef61a22e4830f0f1b0fa4cfe8
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-18 06:38:43 -0300

    Adicionado provider de sessão

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
@@ -7,18 +8,23 @@ import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Dashboard</title>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
-        <meta name="description" content="Controle de Gastos" />
-      </Head>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Dashboard</title>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="shortcut icon" href="/img/favicon.png" />
+          <link rel="apple-touch-icon" href="/img/favicon.png" />
+          <meta name="description" content="Controle de Gastos" />
+        </Head>
 
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
