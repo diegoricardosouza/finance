21427490876e0fc13eecb417a20959eb04dde180
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-21 20:59:03 -0300

    Adicionado pwa ao projeto

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-}
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-module.exports = nextConfig
+module.exports = withPWA({})
