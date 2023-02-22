a8a6615eaae10cbb92f945de7c24df431401ef90
Diego Ricardo <diegoricardoweb@gmail.com>
2023-02-21 21:46:31 -0300

    Ajustado tipagem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,9 +17,13 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <link rel="shortcut icon" href="/img/favicon.png" />
-          <link rel="apple-touch-icon" href="/img/favicon.png" />
+          <link rel="shortcut icon" href="/assets/icon-512.png" />
+          <link rel="apple-touch-icon" href="/assets/icon-512.png" />
+          <link rel="shortcut icon" href="/assets/favicon.png" />
+          <link rel="apple-touch-icon" href="/assets/favicon.png" />
           <meta name="description" content="Controle de Gastos" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#7551FF" />
         </Head>
 
         <GlobalStyles />
--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react'
 import { MdOutlineAttachMoney } from 'react-icons/md'
 import { GiMoneyStack, GiTakeMyMoney } from 'react-icons/gi'
