b0a18f07da46bc37f21e7b8bd60b579165f5b38d
Diego Ricardo <diegoricardoweb@gmail.com>
2023-03-04 22:08:58 -0300

    Adicionado componente Pagination

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,58 @@
+import usePagination from '@/hooks/usePagination'
+import * as S from './styles'
+
+interface PaginationProps {
+  totalPages: number
+  currentPage: number
+  maxPages?: number
+  onPageChange: (page: number) => void
+}
+
+const Pagination = ({
+  totalPages,
+  onPageChange,
+  maxPages = 5,
+  currentPage
+}: PaginationProps) => {
+  const { pages, pagesCount } = usePagination(currentPage, maxPages, totalPages)
+
+  return (
+    <S.PaginationWrapper>
+      <S.PaginationContainer>
+        {currentPage > 1 && (
+          <S.ItemPagination key={0}>
+            <a onClick={() => onPageChange(currentPage - 1)}>«</a>
+          </S.ItemPagination>
+        )}
+
+        {pages.map((page, index) => {
+          if (page === null) {
+            return <S.ItemPaginationDots key={index}>...</S.ItemPaginationDots>
+          }
+
+          const isBeforeCurrentPage = page < currentPage
+          const isAfterCurrentPage = page > currentPage
+          const isCurrentPage = page === currentPage
+
+          const handleClick = () => onPageChange(page)
+
+          return (
+            <S.ItemPagination key={index}>
+              {isBeforeCurrentPage && <a onClick={handleClick}>{page}</a>}
+              {isCurrentPage && <span>{page}</span>}
+              {isAfterCurrentPage && <a onClick={handleClick}>{page}</a>}
+            </S.ItemPagination>
+          )
+        })}
+
+        {currentPage < pagesCount && (
+          <S.ItemPagination key={pagesCount + 1}>
+            <a onClick={() => onPageChange(currentPage + 1)}>»</a>
+          </S.ItemPagination>
+        )}
+      </S.PaginationContainer>
+    </S.PaginationWrapper>
+  )
+}
+
+export default Pagination
---
+++ src/components/Pagination/styles.ts
@@ -0,0 +1,68 @@
+import styled, { css } from 'styled-components'
+
+export const PaginationWrapper = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background: ${theme.colors.primaryLight};
+    border-radius: 2rem;
+    margin-top: 1.7rem;
+    position: relative;
+    overflow: hidden;
+  `}
+`
+
+export const PaginationContainer = styled.ul`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 0;
+  list-style: none;
+  gap: 1rem;
+  padding: 1.5rem 0;
+  flex-wrap: wrap;
+`
+
+export const ItemPagination = styled.li`
+  ${({ theme }) => css`
+    a {
+      display: flex;
+      width: 4rem;
+      height: 4rem;
+      align-items: center;
+      justify-content: center;
+      color: ${theme.colors.white};
+      cursor: pointer;
+      background: ${theme.colors.primary};
+      border-radius: 0.4rem;
+      transition: all 300ms ease-in;
+
+      &:hover {
+        background: ${theme.colors.secondary};
+      }
+    }
+
+    span {
+      display: flex;
+      width: 4rem;
+      height: 4rem;
+      align-items: center;
+      justify-content: center;
+      color: ${theme.colors.white};
+      background: ${theme.colors.secondary};
+      border-radius: 0.4rem;
+    }
+  `}
+`
+
+export const ItemPaginationDots = styled.li`
+  ${({ theme }) => css`
+    display: flex;
+    width: 2rem;
+    height: 4rem;
+    align-items: center;
+    justify-content: center;
+    color: ${theme.colors.white};
+    border-radius: 0.4rem;
+    letter-spacing: 0.2rem;
+  `}
+`
