da49016d3493003f12848868c498c29d03f8d7ed
Diego Ricardo <diegoricardoweb@gmail.com>
2023-03-29 00:18:23 -0300

    Adicionado pagina Freela

---
+++ src/templates/Freela/index.tsx
@@ -0,0 +1,14 @@
+import { toast, ToastContainer } from 'react-toastify'
+import Base from '../Base'
+
+import 'react-toastify/dist/ReactToastify.css'
+
+const Freela = () => {
+  return (
+    <Base title="Freelas" titleBreadcrumb="Freelas">
+      <ToastContainer />
+    </Base>
+  )
+}
+
+export default Freela
